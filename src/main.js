09255e98071726b480cd780dcc36bba511637bc9
Vilenan <vilenan@mail.ru>
2023-01-27 11:22:22 +0300

    удалила из разметки кнопку добавления новой точки, добавила вью кнопки и отрендерила ее в main

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
+import {render} from './framework/render';
 import { getRandomMockPoints, destinationsArray } from './mock/point.js';
 import PointsModel from './model/points-model.js';
 import FilterModel from './model/filter-model.js';
 import FilterPresenter from './presenter/filter-presenter.js';
 import ListPresenter from './presenter/list-presenter.js';
+import NewPointButtonView from './view/new-point-button-view.js';
 
 const siteHeaderEl = document.querySelector('.page-header');
 const filtersEl = siteHeaderEl.querySelector('.trip-controls__filters');
 const siteMainEl = document.querySelector('.page-main');
 const pointsContainerEl = siteMainEl.querySelector('.trip-events');
+const headerContentEl = siteHeaderEl.querySelector('.trip-main');
 
 const pointsModel = new PointsModel({
   points : getRandomMockPoints(),
@@ -15,6 +18,9 @@ const pointsModel = new PointsModel({
 });
 
 const filterModel = new FilterModel();
+
+const newPointButtonComponent = new NewPointButtonView();
+render(newPointButtonComponent, headerContentEl);
 
 const filterPresenter = new FilterPresenter({
   filterContainer: filtersEl,
